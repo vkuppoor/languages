e091684ffcbd3b4da76052c3d6fcc226e4c1b948
Venkat_Kuppoor <venkkuppoor@gmail.com>
2023-05-22 11:59:15 -0400

    implemented rudimentary etop

--- src/etop/mod.rs
+++ src/etop/mod.rs
@@ -1,3 +1,16 @@
+use std::io::{stdin, stdout, Write};
+
 pub fn etop(language: &String) {
-    print!("{language}");
+    println!("language chosen: {}", language);
+    loop {
+        print!("etop # ");
+        stdout().flush().expect("flush error");
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let script = input.trim();
+
+        println!("- : {}", script);
+    }
 }
