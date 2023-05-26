1cd6586fa595b4ab800fa0bf5f51c70b7a7d325b
Venkat_Kuppoor <venkkuppoor@gmail.com>
2023-05-25 20:24:47 -0400

    revamped error handling in parser

--- src/languages/calculator/parser.rs
+++ src/languages/calculator/parser.rs
@@ -1,68 +1,65 @@
-use core::panic;
-
 use super::{Expr, Tok};
+use crate::languages::error::{Error, Result};
+use core::panic;
 
 /** Grammar:
 E -> + N E | - N E | * N E | / N E | N
 N -> -2,147,483,648 | -2,147,483,647 | -2,147,483,646 | ... | 2,147,483,647
 */
-pub fn parser(toks: Vec<Tok>) -> Expr {
+pub fn parser(toks: Vec<Tok>) -> Result<Expr, Tok> {
     let (toks, e) = parse_e(toks);
     if !toks.is_empty() {
-        panic!("toks not empty after parsing")
+        Err(Error::tokens_not_empty(toks))
     } else {
-        e
+        Ok(e)
     }
 }
 
-fn parse_e(toks: Vec<Tok>) -> (Vec<Tok>, Expr) {
+fn parse_e(toks: Vec<Tok>) -> Result<(Vec<Tok>, Expr), Tok> {
     match toks.get(0).unwrap() {
         Tok::TokAdd => {
             let toks = match_token(&toks, Tok::TokAdd);
             let (toks, e1) = parse_e(toks);
             let (toks, e2) = parse_e(toks);
-            (toks, Expr::Add((Box::new(e1), Box::new(e2))))
+            Ok((toks, Expr::Add((Box::new(e1), Box::new(e2)))))
         }
         Tok::TokSub => {
             let toks = match_token(&toks, Tok::TokSub);
             let (toks, e1) = parse_e(toks);
             let (toks, e2) = parse_e(toks);
-            (toks, Expr::Sub((Box::new(e1), Box::new(e2))))
+            Ok((toks, Expr::Sub((Box::new(e1), Box::new(e2)))))
         }
         Tok::TokMult => {
             let toks = match_token(&toks, Tok::TokMult);
             let (toks, e1) = parse_e(toks);
             let (toks, e2) = parse_e(toks);
-            (toks, Expr::Mult((Box::new(e1), Box::new(e2))))
+            Ok((toks, Expr::Mult((Box::new(e1), Box::new(e2)))))
         }
         Tok::TokDiv => {
             let toks = match_token(&toks, Tok::TokDiv);
             let (toks, e1) = parse_e(toks);
             let (toks, e2) = parse_e(toks);
-            (toks, Expr::Div((Box::new(e1), Box::new(e2))))
+            Ok((toks, Expr::Div((Box::new(e1), Box::new(e2)))))
         }
-        Tok::TokInt(_) => parse_n(toks),
+        Tok::TokInt(_) => Ok(parse_n(toks)),
     }
 }
 
-fn parse_n(toks: Vec<Tok>) -> (Vec<Tok>, Expr) {
+fn parse_n(toks: Vec<Tok>) -> Result<(Vec<Tok>, Expr), tok> {
     match toks.get(0).unwrap() {
         Tok::TokInt(i) => {
             let toks = match_token(&toks, Tok::TokInt(*i));
-            (toks, Expr::Int(*i))
+            Ok((toks, Expr::Int(*i)))
         }
-        _ => panic!("Parse_N Error"),
+        _ => Err(Error::production_rule_failure(String::from("N rule"), toks)),
     }
 }
 
-fn match_token(toks: &Vec<Tok>, tok: Tok) -> Vec<Tok> {
+fn match_token(toks: &Vec<Tok>, tok: Tok) -> Result<Vec<Tok>, Tok> {
     match toks.split_first() {
-        None => panic!("Invalid Input: {:?}", tok),
-        Some((h, t)) if *h == tok => t.to_vec(),
-        Some((h, _)) => panic!(
-            "Invalid Input: expected {:?}, from input {:?}, got {:?}",
-            tok, toks, *h
-        ),
+        None => Err(Error::invalid_input(tok)),
+        Some((h, t)) if *h == tok => Ok(t.to_vec()),
+        Some((h, _)) => Err(Error::mismatched_tokens(tok, toks, *h)),
     }
 }
 
@@ -78,7 +75,6 @@ mod tests {
     }
 
     #[test]
-    #[should_panic]
     fn invalid_order() {
         parser(lexer::lexer(&String::from("5 + 8 + 9 8"), 0));
     }
