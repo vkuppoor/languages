e8fdc935089f1d07659bca57996d7a25364921f4
Venkat_Kuppoor <venkkuppoor@gmail.com>
2023-05-28 22:46:21 -0400

    implemented global CalcError for calculator lang to make error handling simpler

--- src/languages/calculator/mod.rs
+++ src/languages/calculator/mod.rs
@@ -1,6 +1,8 @@
 pub mod interpreter;
 pub mod lexer;
 pub mod parser;
+use super::error;
+use std::fmt;
 
 #[derive(PartialEq, Debug, Clone, Copy)]
 pub enum Tok {
@@ -18,4 +20,23 @@ pub enum Expr {
     Mult((Box<Expr>, Box<Expr>)),
     Div((Box<Expr>, Box<Expr>)),
     Int(i32),
+}
+
+#[derive(Debug)]
+pub enum CalculatorError {
+    Parser(error::parser::Error<Tok, Vec<Tok>>),
+    Lexer(error::lexer::Error<String>),
+    Interpreter(error::interpreter::Error),
+}
+
+impl<'a> std::error::Error for CalculatorError {}
+
+impl<'a> fmt::Display for CalculatorError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            CalculatorError::Parser(err) => write!(f, "Parser error: {:?}", err),
+            CalculatorError::Lexer(err) => write!(f, "Lexer error: {:?}", err),
+            CalculatorError::Interpreter(err) => write!(f, "Interpreter error: {:?}", err),
+        }
+    }
 }
